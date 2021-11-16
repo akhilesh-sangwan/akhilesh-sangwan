b48980b12e7e194529e8433a9e5bf25576bc3af3
akhilesh_sangwan <akhilsangwan2000@gmail.com>
2021-11-16 18:20:04 +0530

    commit

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -9,11 +9,11 @@ namespace API.Interfaces
     {
         void Update(AppUser user);
         Task<bool> SaveAllAsync();
-        Task<IEnumerable<AppUser>>GetUsersAsync();
-        Task<AppUser>GetUserByIdAsync(int id);
-        Task<AppUser>GetUserByUsernameAsync(string username);
-        Task<IEnumerable<MemberDto>>GetMembersAsync();
-        Task<MemberDto>GetMemberAsync(string username);
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<MemberDto> GetMemberAsync(string username);
 
     }
 }
